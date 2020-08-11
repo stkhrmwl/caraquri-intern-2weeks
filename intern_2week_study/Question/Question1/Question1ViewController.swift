b3434cade9f0aceaf8dbb1f769d13eb79300d046
TAKAHARA <mwl9dv@gmail.com>
2020-08-11 12:26:43 +0900

    Addを押した際にtextFieldに入っている文字をtextViewに改行して追加

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -10,7 +10,24 @@ final class Question1ViewController: UIViewController {
         super.viewDidLoad()
         self.setValueOnTextView(value: "")
     }
+
+    // addTextButtonイベント
+    @IBAction func addText(_ sender: Any) {
+        let replaceValue = self.getValueOnTextView() + "\n" + self.getValueOnTextField()
+        self.setValueOnTextView(value: replaceValue)
+    }
     
+    // textFieldの文字列を取得する
+    private func getValueOnTextField() -> String {
+        return self.textField.text!
+    }
+    
+    // textViewの文字列を取得する
+    private func getValueOnTextView() -> String {
+        return self.textView.text
+    }
+    
+    // textViewに文字列をセットする
     private func setValueOnTextView(value: String) {
         self.textView.text = value
     }
