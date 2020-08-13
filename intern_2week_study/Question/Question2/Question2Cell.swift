5e37a5f3f5c3486e3e8f70beafee4f1a6f0a5146
TAKAHARA <mwl9dv@gmail.com>
2020-08-13 11:35:41 +0900

    Question2CellクラスにsetLabel，setImageメソッドを追加

--- intern_2week_study/Question/Question2/Question2Cell.swift
+++ intern_2week_study/Question/Question2/Question2Cell.swift
@@ -1,6 +1,15 @@
+import Nuke
 import UIKit
 
 class Question2Cell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var areaImageView: UIImageView!
+    
+    public func setLabel(_ str: String) {
+        label.text = str
+    }
+
+    public func setImage(_ url: URL) {
+        Nuke.loadImage(with: url, into: areaImageView)
+    }
 }
