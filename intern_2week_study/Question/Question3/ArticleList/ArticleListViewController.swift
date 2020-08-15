9059c418d9a70e4543007345b0b4cd972ae628d6
TAKAHARA <mwl9dv@gmail.com>
2020-08-15 17:16:14 +0900

    R.swiftにid: articleListCell未追加

--- intern_2week_study/Question/Question3/ArticleList/ArticleListViewController.swift
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListViewController.swift
@@ -2,4 +2,37 @@ import UIKit
 
 final class ArticleListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var articles: [Article] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "ArticleListCell", bundle: nil), forCellReuseIdentifier: "articleListCell")
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    public func setArticles(articles: [Article]) {
+        self.articles = articles
+    }
+}
+
+extension ArticleListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
+
+extension ArticleListViewController: UITableViewDelegate {
 }
