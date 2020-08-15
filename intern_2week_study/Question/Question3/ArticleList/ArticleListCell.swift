af1b6241218dc9183d7e7462e48aa288687bceef
TAKAHARA <mwl9dv@gmail.com>
2020-08-15 16:30:27 +0900

    ArticleListCell (.swift, .xib)を作成

---
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleListCell.swift
+//  intern_2week_study
+//
+//  Created by TAKAHARA on 2020/08/15.
+//  Copyright © 2020 caraquri. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
