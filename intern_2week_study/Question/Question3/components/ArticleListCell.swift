e3dad28577f735ae55b6981274bc7c435ef6d39a
TAKAHARA <mwl9dv@gmail.com>
2020-08-20 14:56:57 +0900

    可読性向上

--- intern_2week_study/Question/Question3/components/ArticleListCell.swift
+++ intern_2week_study/Question/Question3/components/ArticleListCell.swift
@@ -10,7 +10,8 @@ class ArticleListCell: UITableViewCell {
     public func set(_ article: Article) {
         titleTextLabel.text = article.title
         descriptionTextLabel.text = "LGTM: " +  String(article.likesCount)
-        guard let urlStr = article.user?.profileImageUrl, let url = URL(string: urlStr) else {
+        guard let urlStr = article.user?.profileImageUrl,
+            let url = URL(string: urlStr) else {
             return
         }
         Nuke.loadImage(with: url, into: userImageView)
